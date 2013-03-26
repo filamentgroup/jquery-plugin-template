6f1c1aab6cc7b08c8f6a56ad4338abe33e6e204e
Scott Jehl <scottjehl@gmail.com>
2013-03-26 13:06:53 -0500

    Added optional data-attr overrides of options. Shout-out to @toddparker

--- jquery.boilerplate.js
+++ jquery.boilerplate.js
@@ -1,37 +1,47 @@
 /*
- *  Project:
- *  Description:
- *  Author:
- *  License:
+ * Plugin Name
+ * https://github.com/filamentgroup/PLUGIN-URL
+ * Copyright (c) 2013 Filament Group, Inc.
+ * Licensed under the MIT, GPL licenses.
  */
 
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
 ;(function ($, window, document, undefined) {
-
-    // undefined is used here as the undefined global variable in ECMAScript 3 is
-    // mutable (ie. it can be changed by someone else). undefined isn't really being
-    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-    // can no longer be modified.
-
-    // window and document are passed through as local variable rather than global
-    // as this (slightly) quickens the resolution process and can be more efficiently
-    // minified (especially when both are regularly referenced in your plugin).
-
-    // Create the defaults once
+    
+    // Defaults
     var pluginName = "defaultPluginName";
+    // overrideable defaults
     var defaults = {
         propertyName: "value"
     };
 
-    // The actual plugin constructor
+    // plugin constructor
     function Plugin(element, options) {
         this.element = element;
         // jQuery has an extend method which merges the contents of two or
         // more objects, storing the result in the first object. The first object
         // is generally empty as we don't want to alter the default options for
         // future instances of the plugin
-        this.options = $.extend({}, defaults, options);
+
+        // Allow data-attr option setting?
+        // string and number type defaults can be overridden by data attributes of the same name (with dashes in place of camelCase), if a data-config attribute is present on the widget element.
+        // options set via data attributes will be overridden by options passed directly as arguments
+        if( this.element.is( "[data-config]" ) ){
+            var dataOptions = {};
+            for( var i in defaults ){
+                if( defaults.hasOwnProperty( i ) && ( typeof( defaults[ i ] === "string" ) ) || typeof( defaults[ i ] === "number" ) ){
+                    var dataOption = elem.attr( i.replace( /[A-Z]/g, function( c ) {
+                            return "-" + c.toLowerCase();
+                        }));
+
+                    if( typeof( dataOption ) === "string" || typeof( dataOption ) === "number" ){
+                        dataOptions[ i ] = dataOption;
+                    }
+                }
+            }
+        }
+
+
+        this.options = $.extend( {}, defaults, dataOptions, options );
         this._defaults = defaults;
         this._name = pluginName;
         this.init();
@@ -46,24 +56,29 @@
             // you can add more functions like the one below and
             // call them like so: this.yourOtherFunction(this.element, this.options).
         },
+        // sample method
         yourOtherFunction: function () {
-            // some logic
+            
+        },
+        // sample private method (not actually private, but more for notation purposes)
+        _yourOtherPrivateFunction: function() {
+
         }
     };
 
-    // A really lightweight plugin wrapper around the constructor,
+    // lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
-    $.fn[pluginName] = function (options) {
+    $.fn[ pluginName ] = function (options) {
         return this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+            if ( !$.data( this, "plugin_" + pluginName ) ) {
+                $.data( this, "plugin_" + pluginName, new Plugin( this, options ));
             }
         });
     };
     
-    // A simple auto-init by selector that runs when the dom is ready. Use if desirable.
+    // Simple auto-init by selector that runs when the dom is ready. Use if desirable.
     $(function(){
-        $( "." + pluginName )[pluginName]();
+        $( "." + pluginName )[ pluginName ]();
     });
 
 })(jQuery, window, document);
